3b2da46d4caba287d285abed02f23d0065ed01e6
Patrick Sala <psala@Patricks-Air.lan>
2021-03-07 12:08:03 -0800

    Replaced logo and colors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './full-logo.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit src/App.tsx and save to reload. Coming soon...
+          Coming soon... 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here to learn more
-        </a>
+       
       </header>
     </div>
   );
