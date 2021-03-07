660cb20946342185f4c4dd8f199f93295ec4b00a
Patrick Sala <psala@Patricks-Air.lan>
2021-03-07 11:20:05 -0800

    Changed color of background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Edit src/App.tsx and save to reload. You can try doing this... if you dare </code>
+          Edit src/App.tsx and save to reload. Coming soon...
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to learn more
         </a>
       </header>
     </div>
